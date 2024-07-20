898dcc3d42280710b36ea62879f1335816194bce
anner <anner.wang@icloud.com>
2024-07-21 00:06:59 +0800

    Code Interview Problem

---
+++ templates/Solution.py
@@ -0,0 +1,10 @@
+from typing import Optional
+from typing import List
+from collections import deque
+from collections import defaultdict
+from itertools import pairwise
+
+
+class Solution:
+    def __init__(self) -> None:
+        pass
