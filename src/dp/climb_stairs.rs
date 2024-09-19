5a8b88362ca4fb99c067242fc83c0ab71b0c9aba
anner <anner.wang@icloud.com>
2024-09-20 07:05:43 +0800

    feat: leetcode daily problem

---
+++ src/dp/climb_stairs.rs
@@ -0,0 +1,14 @@
+// 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+// https://leetcode.cn/problems/climbing-stairs/description/?envType=study-plan-v2&envId=top-interview-150
+
+pub fn climb_stairs(n: i32) -> i32 {
+    let mut f0 = 1;
+    let mut f1 = 1;
+    for i in 2..=n {
+        let new_f = f1 + f0;
+        f0 = f1;
+        f1 = new_f;
+    }
+    f1
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 mod greedy;
 mod string;
+mod dp;
