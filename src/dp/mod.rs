742e29eefc478abbdded4bffc6c0831ec3697fdd
anner <anner.wang@icloud.com>
2024-09-26 22:35:07 +0800

    ✨ Leetcode Daily Problem

--- src/dp/mod.rs
+++ src/dp/mod.rs
@@ -1,1 +1,5 @@
 mod climb_stairs;
+mod coin_change;
+mod rob;
+mod word_break;
+struct Solution;
---
+++ src/dp/word_break.rs
@@ -0,0 +1,22 @@
+use super::Solution;
+/**
+ *
+给你一个字符串 s 和一个字符串列表 wordDict 作为字典。如果可以利用字典中出现的一个或多个单词拼接出 s 则返回 true。
+注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用。
+
+输入: s = "applepenapple", wordDict = ["apple", "pen"]
+输出: true
+解释: 返回 true 因为 "applepenapple" 可以由 "apple" "pen" "apple" 拼接成。
+     注意，你可以重复使用字典中的单词。
+
+https://leetcode.cn/problems/word-break/description/?envType=study-plan-v2&envId=top-interview-150
+
+我们定义 dp[i] 表示字符串 s 前 i 个字符组成的字符串 s[0..i−1] 是否能被空格拆分成若干个字典中出现的单词
+
+ */
+impl Solution {
+    #[allow(dead_code)]
+    pub fn word_break(s: String, word_dict: Vec<String>) -> bool {
+        false
+    }
+}
