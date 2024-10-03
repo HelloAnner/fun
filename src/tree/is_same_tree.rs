91fd8fcd4b29a6f3d48b420cbe380dbe3e2c9c63
anner <anner.wang@icloud.com>
2024-10-03 08:44:10 +0800

    ✨ Daily Problem

--- src/tree/is_same_tree.rs
+++ src/tree/is_same_tree.rs
@@ -1,6 +1,13 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
+/**
+给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+
+如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+
+https://leetcode.cn/problems/same-tree/description/
+ */
 use super::{Solution, TreeNode};
 impl Solution {
     #[allow(dead_code)]
--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -21,3 +21,4 @@ impl TreeNode {
 }
 
 mod is_same_tree;
+mod is_symmetric;
