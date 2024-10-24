59cbcafcfba12be53c0a9680064d73ad5176a1c4
anner <anner.wang@icloud.com>
2024-10-24 23:12:57 +0800

    ✨ Daily  Rewrite

--- src/recursion/mod.rs
+++ src/recursion/mod.rs
@@ -1,3 +1,19 @@
 mod gray_code;
+mod merge_two_lists;
 
 struct Solution;
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    #[allow(dead_code)]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
