eff29e1f474ae7d1b1f24be552b005e5c10b78d6
anner <anner.wang@icloud.com>
2024-11-02 20:49:43 +0800

    feat: daily subject

--- src/backtracking/combination_sum2.rs
+++ src/backtracking/combination_sum2.rs
@@ -4,7 +4,7 @@ use crate::backtracking::Solution;
 给定一个候选人编号的集合 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
 candidates 中的每个数字在每个组合中只能使用 一次 。
 注意：解集不能包含重复的组合。
-
+https://leetcode.cn/problems/combination-sum-ii/description/
 */
 impl Solution {
     pub fn combination_sum2(mut candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
--- src/backtracking/mod.rs
+++ src/backtracking/mod.rs
@@ -1,5 +1,6 @@
 mod combination_sum;
 mod combination_sum2;
 mod combine;
+mod subsets;
 
 struct Solution;
