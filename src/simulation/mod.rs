3af6d1e18690d8222cc4107406b85d59e518ff7a
anner <anner.wang@icloud.com>
2024-11-04 07:51:30 +0800

    feat: daily leetcode problem

--- src/simulation/mod.rs
+++ src/simulation/mod.rs
@@ -2,6 +2,7 @@ mod cal_points;
 mod check_record;
 mod difference_of_sum;
 mod find_winning_player;
+mod judge_square_sum;
 mod max_height_of_triangle;
 mod max_score_sightseeing_pair;
 mod min_operations;
