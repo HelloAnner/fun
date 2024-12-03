d0c6a47ecf01a6d675ef6e15a0c50c86baec6256
anner <anner.wang@icloud.com>
2024-12-03 08:24:48 +0800

    feat: daily leetcode problem
    Signed-off-by: anner <anner.wang@icloud.com>

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,7 +1,8 @@
+mod can_alice_win;
+mod check_two_chessboards;
 mod distinct_names;
 mod nth_person_gets_nth_seat;
 mod smallest_range_i;
 mod smallest_range_ii;
-mod can_alice_win;
 
 struct Solution;
